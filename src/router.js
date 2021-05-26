37fcf5bb344471a1aa6ac0854704184b129ea44a
Faichuis <465766401@qq.com>
2021-05-27 00:39:14 +0800

    0527

--- src/router.js
+++ src/router.js
@@ -29,26 +29,6 @@ export default new Router({
             path: '/register',
             name: 'Register',	//可以不用name字段   但有时需要用到name匹配
             component: () => import('./pages/register.vue')
-        } ,
-        {
-            path: '/write',
-            name: 'write',
-            component: () => import('./components/index/Write.vue')
-        },
-        {
-            path: '/receive',
-            name: 'receive',
-            component: () => import('./components/index/Receive.vue')
-        },
-        {
-            path: '/manual',
-            name: 'manual',
-            component: () => import('./components/index/Manual.vue')
-        },
-        {
-            path: '/send',
-            name: 'send',
-            component: () => import('./components/index/Send.vue')
         },
     ]
 });
